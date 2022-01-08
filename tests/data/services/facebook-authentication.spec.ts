4c98e025c2bade85d7646df2c19ade61e51dc0ad
Nichollas Falcão dos Santos <nichollas.falcao@tallos.com.br>
2022-01-08 20:07:34 -0300

    Ensure Service calls LoadFacebookUserApi once

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -4,12 +4,15 @@ import { AuthenticationException } from '@/domain/exceptions'
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
+  callsCount = 0
   result = undefined
 
   async loadUser(
     params: LoadFacebookUserApi.Params
   ): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
+    this.callsCount++
+
     return this.result
   }
 }
@@ -22,6 +25,7 @@ describe('FacebookAuthenticationService', () => {
     await sut.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApi.token).toBe('any_token')
+    expect(loadFacebookUserApi.callsCount).toBe(1)
   })
 
   it('Should return AuthenticationException when LoadFacebookUserApi returns undefined', async () => {
