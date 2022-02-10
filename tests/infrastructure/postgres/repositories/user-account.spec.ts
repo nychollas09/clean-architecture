f8a958e0d0eecf7b6183119e4a657ce867f1065f
Nichollas Falcão <nychollas2013@hotmail.com>
2022-02-09 23:28:41 -0300

    Ensure PostgresUserAccountRepository returns undefined if email does not exists

--- tests/infrastructure/postgres/repositories/user-account.spec.ts
+++ tests/infrastructure/postgres/repositories/user-account.spec.ts
@@ -12,7 +12,7 @@ describe('PostgresUserAccountRepository', () => {
     sut = new PostgresUserAccountRepository()
   })
 
-  describe('LoadUserAccount', () => {
+  describe('load', () => {
     it('Should return an account if email exists', async () => {
       const db = newDb()
 
@@ -30,8 +30,27 @@ describe('PostgresUserAccountRepository', () => {
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
+
+      await connection.close()
+    })
+
+    it('Should return undefined if email does not exists', async () => {
+      const db = newDb()
+
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PostgresUserAccount]
+      })
+
+      await connection.synchronize()
+
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
+
+      await connection.close()
     })
   })
 
-  describe('SaveUserAccount', () => {})
+  describe('save', () => {})
 })
