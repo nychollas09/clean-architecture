dc7a8f6f7118fdf2c5163a31428964b6e3e7379a
Nichollas Falcão <nychollas2013@hotmail.com>
2022-02-11 11:43:12 -0300

    Ensure FacebookLoginController returns 200 if authentication suceeds

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,5 +1,6 @@
 import { AuthenticationException } from '@/domain/exceptions'
 import { FacebookAuthentication } from '@/domain/features'
+import { AccessToken } from '@/domain/models'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 type HttpResponse = { statusCode: number, data: any }
@@ -25,10 +26,11 @@ export class FacebookLoginController {
       token: httpRequest.token
     })
 
-    return {
-      statusCode: 401,
-      data: result
+    if (result instanceof AccessToken) {
+      return { statusCode: 200, data: { accessToken: result.value } }
     }
+
+    return { statusCode: 401, data: result }
   }
 }
 
@@ -38,6 +40,7 @@ describe('FacebookLoginController', () => {
 
   beforeAll(() => {
     facebookAuth = mock<FacebookAuthentication>()
+    facebookAuth.perform.mockResolvedValue(new AccessToken('any_value'))
   })
 
   beforeEach(() => {
@@ -85,6 +88,17 @@ describe('FacebookLoginController', () => {
     expect(httpResponse).toEqual({
       statusCode: 401,
       data: new AuthenticationException()
+    })
+  })
+
+  it('Should return 200 if authentication succeeds', async () => {
+    const httpResponse = await sut.handle({ token: 'any_token' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: {
+        accessToken: 'any_value'
+      }
     })
   })
 })
