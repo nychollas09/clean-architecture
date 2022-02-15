5d1fe2d0cd0dc6d97f1e42f7924d3ac91e475cf5
Nichollas Falcão <nychollas2013@hotmail.com>
2022-02-14 23:58:17 -0300

    Ensure ValidationComposite returns the first error found

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -26,4 +26,13 @@ describe('ValidationComposite', () => {
 
     expect(error).toBeUndefined()
   })
+
+  it('Should return the first error', () => {
+    validator1.validate.mockReturnValue(new Error('error_1'))
+    validator2.validate.mockReturnValue(new Error('error_2'))
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
+  })
 })
