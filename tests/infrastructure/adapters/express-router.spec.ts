fdd51b0e7b5bd8f3cd6bf048ec862bdc3701d34f
Nichollas Falcão <nychollas2013@hotmail.com>
2022-02-15 23:09:58 -0300

    Esnure ExpressRouterAdapter responds with correct status code and error

--- tests/infrastructure/adapters/express-router.spec.ts
+++ tests/infrastructure/adapters/express-router.spec.ts
@@ -7,7 +7,15 @@ export class ExpressRouterAdapter {
   constructor(private readonly controller: Controller) {}
 
   public async adapt(request: Request, response: Response): Promise<void> {
-    await this.controller.handle(request.body)
+    const httpResponse = await this.controller.handle(request.body)
+
+    response
+      .status(httpResponse.statusCode)
+      .json(
+        httpResponse.data instanceof Error
+          ? { error: httpResponse.data.message }
+          : httpResponse.data
+      )
   }
 }
 
@@ -15,11 +23,15 @@ describe('ExpressRouterAdapter', () => {
   let sut: ExpressRouterAdapter
   let controller: MockProxy<Controller>
 
-  beforeAll(() => {
-    controller = mock<Controller>()
-  })
-
   beforeEach(() => {
+    controller = mock<Controller>()
+    controller.handle.mockResolvedValue({
+      statusCode: 200,
+      data: {
+        data: 'any_data'
+      }
+    })
+
     sut = new ExpressRouterAdapter(controller)
   })
 
@@ -30,5 +42,50 @@ describe('ExpressRouterAdapter', () => {
     await sut.adapt(request, response)
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+    expect(controller.handle).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should call handle with empty request', async () => {
+    const request = getMockReq()
+    const response = getMockRes().res
+
+    await sut.adapt(request, response)
+
+    expect(controller.handle).toHaveBeenCalledWith({})
+    expect(controller.handle).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should respond with 200 and valid data', async () => {
+    const request = getMockReq()
+    const response = getMockRes().res
+
+    await sut.adapt(request, response)
+
+    expect(response.status).toHaveBeenCalledWith(200)
+    expect(response.json).toHaveBeenCalledWith({
+      data: 'any_data'
+    })
+    expect(response.status).toHaveBeenCalledTimes(1)
+    expect(response.json).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should respond valid error', async () => {
+    const error = {
+      statusCode: 400,
+      data: new Error('any_error')
+    }
+    controller.handle.mockResolvedValueOnce(error)
+
+    const request = getMockReq()
+    const response = getMockRes().res
+
+    await sut.adapt(request, response)
+
+    expect(response.status).toHaveBeenCalledWith(error.statusCode)
+    expect(response.json).toHaveBeenCalledWith({
+      error: error.data.message
+    })
+    expect(response.status).toHaveBeenCalledTimes(1)
+    expect(response.json).toHaveBeenCalledTimes(1)
   })
 })
